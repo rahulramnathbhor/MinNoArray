54f1917d16b2f78cd33e107c799acd5393f5c266
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-21 17:00:12 +0530

    Add project files.

---
+++ MinNoArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinNoArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+            int min = numbers[0];
+           
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+            }
+            Console.Write("Minimum number is :" + min);
+
+        }
+    }
+}
